0616df00d6ba139df3bd0c94fc6eeb9db3262609
Jackson Stephens <filthee@gmail.com>
2014-05-23 18:10:32 -0400

    PR template and responsive header

--- Build/js/main.js
+++ Build/js/main.js
@@ -1,31 +1,46 @@
 $(document).ready(function() {
+
+
+	if (Modernizr.mq('only screen and (min-width: 767px)')) {
+
+	// Development only - remove before deploying to production
+		$('header a').click(function() {
+			event.preventDefault();
+		});
+
+	}
 	
+	if (Modernizr.mq('only screen and (max-width: 768px)')) { // only execute if on phone or tablet screen sizes
+	
+		// Menu toggle
+		$('.toggle-nav').click(function() {
+			$('.inner-wrapper').toggleClass('global-nav-is-opened');
+			$('.main-body').toggleClass('transparent');
+			$('footer').toggleClass('transparent');
+			$('.header-branding').toggleClass('transparent');
+			$(this).toggleClass('rotate-clockwise-90-degrees');
+			event.preventDefault();
+		});
 
-	// Menu toggle
-	$('.toggle-nav').click(function() {
-		$('.wrapper').toggleClass('global-nav-is-opened');
-		$('.main-body').toggleClass('transparent');
-		$(this).toggleClass('rotate-clockwise-90-degrees');
-		event.preventDefault();
-	});
+		// Menu item expand
+		$('.global-nav > ul > li > a').click(function() {
+			
+			var clickedSubMenu = $(this).next('.global-nav-sub-menu');
 
-	// Menu item expand
-	$('.global-nav > ul a').click(function() {
-		
-		var clickedSubMenu = $(this).next('.global-nav-sub-menu');
+			// Hide all sub menus except for the one clicked
+			$('.global-nav-sub-menu').not(clickedSubMenu).addClass('visually-hidden');
 
-		// Hide all sub menus except for the one clicked
-		$('.global-nav-sub-menu').not(clickedSubMenu).addClass('visuallyhidden');
+			// Toggle visibility of clicked submenu
+			if($(clickedSubMenu).hasClass('visually-hidden')) {
+				clickedSubMenu.removeClass('visually-hidden');
+			} else {
+				clickedSubMenu.addClass('visually-hidden');
+			}
 
-		// Toggle visibility of clicked submenu
-		if($(clickedSubMenu).hasClass('visuallyhidden')) {
-			clickedSubMenu.removeClass('visuallyhidden');
-		} else {
-			clickedSubMenu.addClass('visuallyhidden');
-		}
+			event.preventDefault();
+		});
 
-		event.preventDefault();
-	});
+	}
 
 
 });
